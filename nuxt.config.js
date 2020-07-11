41204729d9c15d0459d489a7c3658d7aa73dadcf
Darwin <darwinblacks@gmail.com>
2020-07-11 10:48:59 +0700

    fix: add google font in assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,19 +62,7 @@ const config = {
         content: 'https://darwin1224.netlify.app',
       },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800',
-      },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700',
-      },
-    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   css: ['@/assets/main.scss'],
   plugins: [],
